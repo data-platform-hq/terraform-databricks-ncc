949313a1e8a3f0786bce2bcbda465dc75f977dc2
oleh_mykolaishyn <oleh_mykolaishyn@epam.com>
2024-05-25 21:49:47 +0300

    feat: network connectivity config

---
+++ main.tf
@@ -0,0 +1,23 @@
+locals {
+  ncc_config_mapped = {
+    for object in flatten([for sa in var.storage_accounts : [for kind in ["blob", "dfs"] : {
+      name = "${sa.name}-${kind}"
+      kind = kind
+      id   = sa.id
+  }]]) : object.name => object }
+}
+
+resource "databricks_mws_ncc_binding" "this" {
+  network_connectivity_config_id = var.network_connectivity_config_id
+  workspace_id                   = var.databricks_workspace_id
+}
+
+resource "databricks_mws_ncc_private_endpoint_rule" "this" {
+  for_each = local.ncc_config_mapped
+
+  network_connectivity_config_id = var.network_connectivity_config_id
+  resource_id                    = each.value.id
+  group_id                       = each.value.kind
+
+  depends_on = [databricks_mws_ncc_binding.this]
+}
